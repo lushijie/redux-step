96cd66144317e15b28997ebb55f8fc47b2fd615b
lushijie <lushijie1218@126.com>
2016-11-15 10:15:20 +0800

    f

--- examples/step5/index.jsx
+++ examples/step5/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import {render} from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import Counter from './components/counter';
-//import * as counterAction from './actions/counter';
 import counterReducer from './reducers/counter-index';
 
 const store = createStore(counterReducer, {concatState: 6, calcState: 10});
